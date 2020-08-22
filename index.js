894b3d393d1972abfc7b275a3d460d550defa378
Darlan Alves <me@darlanalv.es>
2020-08-23 00:37:07 +0200

    feat: update to v2

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ import MarkdownIt from 'markdown-it';
 
 const markdown = new MarkdownIt();
 const configuration = {
-  version: 1,
-  input: Format.Text,
-  output: Format.Text,
+  version: 2,
+  actions: {
+    default: {
+      input: Format.Text,
+      output: Format.Text,
+      handler: (input, output) => output.send(markdown.render(input.body)),
+    },
+  },
 };
 
-lambda(configuration, (input, output) => output.send(markdown.render(input.body)));
+lambda(configuration);
